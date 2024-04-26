f2a490702443f9bd487a68c47c739a19d19088de
UKvemula <uvemulap@cbu.edu>
2024-04-26 17:56:18 -0400

    m

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -1,17 +1,14 @@
 """ex_5_1.py"""
 import argparse
-try:
-    from src.ex_5_0 import line_count
-except ImportError:
-    from ex_5_0 import line_count
-
+from ex_5_0 import line_count
 
 def main():
-    parser = argparse.ArgumentParser(description="This program prints the number of lines in infile.")
+    parser = argparse.ArgumentParser(description="This program prints the number of lines in the input file.")
     parser.add_argument('infile', help='Input file name')
     args = parser.parse_args()
 
-    line_count(args.infile)
+    line_count_value = line_count(args.infile)
+    print(f"Number of lines in {args.infile}: {line_count_value}")
 
 
 
--- tests/test_ex_5_1.py
+++ tests/test_ex_5_1.py
@@ -1,14 +1,9 @@
 import os
 from subprocess import run, PIPE
-import pytest
-
-MODULE_PATH = os.path.join(os.path.dirname(__file__), '..', 'src', 'ex_5_1.py')
-
 
 def test_ex_5_1_has_description(capfd):
-    run_command = run(['python', MODULE_PATH, '-h'], stdout=PIPE, stderr=PIPE, text=True)
+    run_command = run(['python', 'ex_5_1.py', '-h'], stdout=PIPE, stderr=PIPE, text=True)
     assert "prints the number of lines" in run_command.stdout.lower()
-
 
 def test_ex_5_1_prints_correct_line_count(capfd):
     infile_fixture = os.path.join(
@@ -16,5 +11,6 @@ def test_ex_5_1_prints_correct_line_count(capfd):
         "fixtures",
         "ex_5_0_fixture.txt",
     )
-    run_command = run(['python', MODULE_PATH, infile_fixture], stdout=PIPE, stderr=PIPE, text=True)
-    assert run_command.stdout.strip() == f"Number of lines in {infile_fixture}: 4"
+    run_command = run(['python', 'ex_5_1.py', infile_fixture], stdout=PIPE, stderr=PIPE, text=True)
+    expected_output = f"Number of lines in {infile_fixture}: 4"
+    assert run_command.stdout.strip() == expected_output
